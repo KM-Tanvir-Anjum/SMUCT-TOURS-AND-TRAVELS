cd08169273913c01c0d6c74a06577fd769e9b38e
KM-Tanvir-Anjum <tanvirnp20@gmail.com>
2023-10-03 15:24:35 +0600

    3/10

---
+++ app/src/main/java/com/tanvir/tt/user/AppHomeActivity.java
@@ -0,0 +1,16 @@
+package com.tanvir.tt.user;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.tanvir.tt.R;
+
+public class AppHomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_app_home);
+    }
+}
